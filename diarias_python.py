fdc1a3283a7ddbdfc8b1978981c120be71384b35
Joao <147270379+11jaum@users.noreply.github.com>
2025-03-28 21:29:46 -0300

    Update diarias_python.py

--- diarias_python.py
+++ diarias_python.py
@@ -1,11 +1,11 @@
 S = 255.50
-D = 305,50
-T = 360,50
+D = 305.50
+T = 360.50
 
 print("Bem vindo! \n Tipos de diárias disponiveis: \n S - Simples (Valor da diária: R$255,50) \n D - Duplo (Valor da diária: R$305,50) \n T - Triplo (Valor da diária: R$360,50) \n")
 
-td = input('Qual tipo de diária você quer?') 
-qd = int(input('Quantas diárias você quer?'))
+td = input('Qual tipo de diária você quer? ') 
+qd = int(input('Quantas diárias você quer? '))
 
 if td == 'S':
   total = qd * S
